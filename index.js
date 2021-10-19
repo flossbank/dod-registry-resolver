db1c185596641a75476b338a67d9e1d2a1ba07cd
Joel Wasserman <joel.wasserman8@gmail.com>
2021-10-19 16:07:08 -0700

    first pass

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const Process = require('./lib/process')
 const Config = require('./lib/config')
 const Db = require('./lib/mongo')
 const Dynamo = require('./lib/dynamo')
-const GitHub = require('./lib/github')
 
 const kms = new AWS.KMS({ region: 'us-west-2' })
 const docs = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' })
 
 /*
-- 
+- Fetches each language/registry top level packages json using correlation id from s3
+- Fetch no-comp list from mongo and fetch all deps
+- Write package weights map to s3
+- Send off event to dod-distribute-donations
 */
 exports.handler = async (event) => {
   const log = Pino()
   const dynamo = new Dynamo({ log, docs })
   const config = new Config({ log, kms, dynamo })
-
-  const retriever = new GitHub({ log, config })
-  await retriever.init()
 
   const db = new Db({ log, config })
   await db.connect()
@@ -30,7 +29,7 @@ exports.handler = async (event) => {
   let results
   try {
     results = await Promise.all(
-      event.Records.map(record => Process.process({ record, db, dynamo, resolver, retriever, log }))
+      event.Records.map(record => Process.process({ record, db, dynamo, resolver, log }))
     )
     if (!results.every(result => result.success)) {
       throw new Error(JSON.stringify(results))
