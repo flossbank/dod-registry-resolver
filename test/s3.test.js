13d99c7ce3776a0e6a158530cb6446cc1c7877f1
Peter Squicciarini <stripedpajamas273@gmail.com>
2021-10-20 15:04:47 -0700

    Add missing s3 function

--- test/s3.test.js
+++ test/s3.test.js
@@ -5,6 +5,9 @@ const S3 = require('../lib/s3')
 test.beforeEach((t) => {
   t.context.s3 = new S3({
     s3: {
+      putObject: sinon.stub().returns({
+        promise: sinon.stub().resolves()
+      }),
       getObject: sinon.stub().returns({
         promise: sinon.stub()
           .onFirstCall().resolves({
@@ -46,5 +49,25 @@ test('getTopLevelPackages', async (t) => {
   t.deepEqual(t.context.s3.s3.getObject.secondCall.args, [{
     Bucket: 'org-donation-state',
     Key: 'asdf/ruby_rubygems_top_level_packages.json'
+  }])
+})
+
+test('putPackageWeightMaps', async (t) => {
+  await t.context.s3.putPackageWeightMaps({
+    correlationId: 'asdf',
+    packageWeightMaps: [
+      { language: 'javascript', registry: 'npm', weightMap: new Map([['standard', 1], ['js-deep-equals', 2]]) },
+      { language: 'ruby', registry: 'rubygems', weightMap: new Map([['json', 1], ['json-something', 2]]) }
+    ]
+  })
+  t.deepEqual(t.context.s3.s3.putObject.firstCall.args, [{
+    Body: JSON.stringify([['standard', 1], ['js-deep-equals', 2]]),
+    Bucket: 'org-donation-state',
+    Key: 'asdf/javascript_npm_package_weight_map.json'
+  }])
+  t.deepEqual(t.context.s3.s3.putObject.secondCall.args, [{
+    Body: JSON.stringify([['json', 1], ['json-something', 2]]),
+    Bucket: 'org-donation-state',
+    Key: 'asdf/ruby_rubygems_package_weight_map.json'
   }])
 })
